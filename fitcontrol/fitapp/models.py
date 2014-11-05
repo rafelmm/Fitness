b17498cae7d71c4072217f4557659758d03d4896
rafelmm <rafelmm@gmail.com>
2014-11-05 09:45:04 +0100

    Se ha unificado el modelo para usuarios. Se ha completado model Direccion

--- fitcontrol/fitapp/models.py
+++ fitcontrol/fitapp/models.py
@@ -2,26 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# Dirección: contiene los campos necesarios para guardar la dirección postal
 class Direccion(models.Model):
     calle = models.CharField(max_length=150, blank=True)
     numero = models.CharField(max_length=20, blank=True)
-    locality = models.CharField(max_length=20, blank=True)
+    piso = models.CharField(max_length=5, blank=True)
+    puerta = models.CharField(max_length=5, blank=True)
+    escalera = models.CharField(max_length=5, blank=True)
+    localidad = models.CharField(max_length=20, blank=True)
     cp = models.PositiveIntegerField(null=False)
 
     def __str__(self):
-        return '%s %s,%d %s' %(self.calle, self.numero, self.cp, self.locality,)
-    
-class Entrenador(models.Model):
+        camposExtra = ''
+        if (self.escalera != ''):
+            camposExtra = ' esc. %s' % self.escalera
+        if (self.piso != ''):
+            camposExtra = '%s %s' % (camposExtra, self.piso,)
+        if (self.puerta != ''):
+            camposExtra = '%s%s' % (camposExtra, self.puerta,)
+        return '%s %s%s,%d %s' %(self.calle, self.numero, camposExtra, self.cp, self.locality,)
+
+# Usuario: Pueden ser de distintos tipos segun al grupo al que pertenece
+#   - Entrenador
+#   - Socio
+#   - Gestor
+class Usuario(models.Model):
     user = models.OneToOneField(User)
     direccion = models.ForeignKey(Direccion)
     telefono = models.CharField(max_length=15, blank=True)
-    
-class Socio(models.Model):
-    user = models.OneToOneField(User)
-    direccion = models.ForeignKey(Direccion)
-    telefono = models.CharField(max_length=15, blank=True)
-    
-class Gestor(models.Model):
-    user = models.OneToOneField(User)
-    direccion = models.ForeignKey(Direccion)
-    telefono = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return user.get_full_name()
