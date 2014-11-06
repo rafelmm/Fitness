a7351fc275947af19ca2724eabd260066c6919e4
rafelmm <rafelmm@gmail.com>
2014-11-06 16:57:07 +0100

    Base de datos mysql

--- fitcontrol/fitcontrol/settings.py
+++ fitcontrol/fitcontrol/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'fitcontrol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'NAME':'fitcontrol',
     }
 }
 
