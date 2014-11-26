d0b661d69557d6b8a3616269474013d0bab21d32
rafelmm <rafelmm@gmail.com>
2014-11-26 23:57:07 +0100

    Tutorial 5, vistas genericas y formularios

--- fitcontrol/fitapp/urls.py
+++ fitcontrol/fitapp/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from fitapp import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'user/(?P<user_id>\d+)/$', views.user_detail, name='user_detail'),
+urlpatterns = [
+    url(r'^$', views.IndexView, name='index'),
+    url(r'user/(?P<pk>\d+)/$', views.UserDetailView.as_view(), name='user_detail'),
     url(r'address/$', views.address_list, name='address_list'),
-)
+    url(r'^user/(?P<pk>\d+)/update/$', views.UserUpdateView.as_view(), name='user_update'),
+    url(r'^user/(?P<usuario_id>\d+)/update_action/$', views.user_update_action, name='user_update_action'),
+]
--- fitcontrol/fitapp/views.py
+++ fitcontrol/fitapp/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
 from fitapp.models import Direccion, Usuario
 
-# Create your views here.
-def index(request):
-    return render(request, 'fitapp/index.html', {})
+def IndexView(request):
+    return render(request, 'fitapp/index.html')
 
-def user_detail(request, user_id):
+class UserDetailView(generic.DetailView):
+    model = Usuario
+    template_name = 'fitapp/user_detail.html'
+
+class UserUpdateView(generic.DetailView):
+    model = Usuario
+    template_name = 'fitapp/user_update.html'
     
-    usuario = get_object_or_404(Usuario, pk=user_id)
-    context = {
-        'usuario':  usuario,
-    }
-    return render(request, 'fitapp/user_detail.html', context)
-
+def user_update_action(request, usuario_id):
+    u = get_object_or_404(Usuario, pk=usuario_id)
+    try:
+        nombre = request.POST['usuario-nombre']
+        apellidos = request.POST['usuario-apellidos']
+        dni = request.POST['usuario-dni']
+        u.user.first_name = nombre
+        u.user.last_name = apellidos
+        u.dni = dni
+        u.user.save()
+        u.save()
+    except:
+        return render(request, 'fitapp/user_detail.html', {
+            'error_message': "No se ha podido actualizar el usuario.",
+        })
+    else:
+        return HttpResponseRedirect(reverse('fitapp:user_detail', args=(u.id,)))
+    
 def address_list(request):
     address_list = Direccion.objects.all()
     context = {'address_list': address_list}
