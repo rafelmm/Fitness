a865ebab20a0eb90f638844ef5582ef4588fedfd
rafelmm <rafelmm@gmail.com>
2014-11-27 23:18:37 +0100

    Creado esquema de navegación Creado formulario de actualización de direcciones

--- fitcontrol/fitapp/urls.py
+++ fitcontrol/fitapp/urls.py
@@ -3,8 +3,10 @@ from fitapp import views
 
 urlpatterns = [
     url(r'^$', views.IndexView, name='index'),
-    url(r'user/(?P<pk>\d+)/$', views.UserDetailView.as_view(), name='user_detail'),
-    url(r'address/$', views.address_list, name='address_list'),
+    url(r'^user/list/$', views.UserListView.as_view(), name='user_list'),
+    url(r'^user/(?P<pk>\d+)/$', views.UserDetailView.as_view(), name='user_detail'),
     url(r'^user/(?P<pk>\d+)/update/$', views.UserUpdateView.as_view(), name='user_update'),
     url(r'^user/(?P<usuario_id>\d+)/update_action/$', views.user_update_action, name='user_update_action'),
+    url(r'^address/(?P<pk>\d+)/update/$', views.AddressUpdateView.as_view(), name='address_update'),
+    url(r'^address/(?P<direccion_id>\d+)/update_action/$', views.address_update_action, name='address_update_action'),
 ]
--- fitcontrol/fitapp/views.py
+++ fitcontrol/fitapp/views.py
@@ -8,6 +8,14 @@ from fitapp.models import Direccion, Usuario
 def IndexView(request):
     return render(request, 'fitapp/index.html')
 
+class UserListView(generic.ListView):
+    template_name = 'fitapp/user_list.html'
+    context_object_name = 'user_list'
+
+    def get_queryset(self):
+        """Return all the users."""
+        return Usuario.objects.all()
+    
 class UserDetailView(generic.DetailView):
     model = Usuario
     template_name = 'fitapp/user_detail.html'
@@ -33,9 +41,27 @@ def user_update_action(request, usuario_id):
         })
     else:
         return HttpResponseRedirect(reverse('fitapp:user_detail', args=(u.id,)))
-    
-def address_list(request):
-    address_list = Direccion.objects.all()
-    context = {'address_list': address_list}
-    return render(request, 'fitapp/address_list.html', context)
 
+
+class AddressUpdateView(generic.DetailView):
+    model = Direccion
+    template_name = 'fitapp/address_update.html'
+    
+def address_update_action(request, direccion_id):
+    d = get_object_or_404(Direccion, pk=direccion_id)
+    try:
+        d.calle = request.POST['direccion-calle']
+        d.numero = request.POST['direccion-numero']
+        d.piso = request.POST['direccion-piso']
+        d.puerta = request.POST['direccion-puerta']
+        d.escalera = request.POST['direccion-escalera']
+        d.localidad = request.POST['direccion-localidad']
+        d.cp = request.POST['direccion-cp']
+        d.pais = request.POST['direccion-pais']
+        d.save()
+    except:
+        return render(request, 'fitapp/address_update.html', {
+            'error_message': "No se ha podido actualizar la direccion.",
+        })
+    else:
+        return HttpResponseRedirect(reverse('fitapp:user_detail', args=(d.user.id,)))
